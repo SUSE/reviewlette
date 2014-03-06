82f1e534aadbe600edfa5a15148f4834f515ae98
jschmid1 <jschmid@suse.de>
2014-03-06 13:18:16 +0100

    updated

--- spec/reviewlette_spec.rb
+++ spec/reviewlette_spec.rb
@@ -7,13 +7,11 @@ describe 'reviewlette' do
 
   it 'return jschmid' do 
     expect(subject.namereturn).to eq( 'jschmid' )
-    end 
-  it 'exp calc' do
-    expect(subject.exp(3)).to eq ( 9 )
   end 
-  it 'random name return' do 
-    expect(subject.randomname).to be_instance_of(String) 
-    end
+  it 'read from file and sample' do 
+    expect(subject.readfromfile).not_to be_nil
+    expect(subject.readfromfile).to be_instance_of(String) 
+  end
   it 'i picked this random name for you' do
     expect(subject.namecall).to be_instance_of(String)
   end
@@ -21,6 +19,5 @@ describe 'reviewlette' do
   it 'ensures that there is always a name to output' do
     subject.namecall
     expect(subject.name).not_to be_nil
-
   end
 end
--- test.rb
+++ test.rb
@@ -1,32 +1,29 @@
+require 'yaml'
+
 class Reviewlette
-
   attr_accessor :name
-
-  def initialize
-    
-  end
   def namereturn
     'jschmid'
   end
-  def exp(zahl)
-    zahl * zahl
-  end
-  def randomname
-    @namelist = ['jschmid', 'fbayerlein', 'dbamberger', 'tschmidt', 'sschubert', 'vlewin', 'achernikov', 'wspepahnson']
-    @name = @namelist.sample
+  def readfromfile
+    config = YAML.load_file("names.yml")
+    @allnames = config
+    @name = @allnames.sample
   end
   def namecall
-    randomname unless @name
+  readfromfile unless @name
    "its your turn #{@name}"
   end
 end
 
 roulette= Reviewlette.new
+roulette.readfromfile
 roulette.namecall
 
 
 
-
+#do the yml file handling with an api -> next step 
+#see git-review 
 	
 
 
