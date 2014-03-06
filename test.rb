58f4b68c5fc6f4846e45cfbd8cb7614d2f8c5baf
Fnoopix <fnoopix@gmail.com>
2014-03-06 17:04:26 +0100

    Trying to add some activitys

--- test.rb
+++ test.rb
@@ -3,6 +3,7 @@ require 'yaml'
 
 class Reviewlette
   attr_accessor :name
+  attr_accessor :activity
   def namereturn
     'jschmid'
   end
@@ -14,12 +15,31 @@ class Reviewlette
   def namecall
   readfromfile unless @name
    "its your turn #{@name}"
+   "do you have time for this? #{@name}" 
+   "Yes or No. Type in y or n"
+  end 
+  def activitystodo
+    config = YAML.load_file("activitys.yml")
+    @allactivity = config
+    @activity = @allactivity.new
+  end
+  def askuser
+   response = gets 
+   response = response.chomp
+   readfromfile unless @activity
+   if response == "y" then
+    "Well then #{@activity}"
+   else
+   "Oke bring me someone else"
+   end 
   end
 end
 
 roulette= Reviewlette.new
 roulette.readfromfile
+roulette.activitystodo
 puts roulette.namecall
+puts roulette.askuser
 
 
 
