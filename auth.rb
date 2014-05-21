58653f1f2a4a32db63e26005a248fb151ebae3bc
jschmid1 <jschmid@suse.de>
2014-05-21 14:28:50 +0200

    ...

--- auth.rb
+++ auth.rb
@@ -22,18 +22,3 @@ end
 
 
 
-
-############ OPTIONAL AUTH ###############
-
-
-#client.create_authorization(:scopes => ["user"], :note => "Name of token")
-#client = Octokit::Client.new \
-#    :client_id     => "24f4471999a562f33e5c",
-#    :client_secret => "5f5a34f69ec94f34c28fe1ec33303851329dc4c6"
-
-#user = Octokit.user 'jschmid1'
-
-## or better use the token?
-
-#client = Octokit::Client.new(:access_token=>"f4dc4255b28faf21e80d81ea07748fccff25e147")
-
--- main.rb
+++ main.rb
@@ -3,19 +3,25 @@ require 'debugger'
 require '/home/jschmid/reviewlette/mail'
 require '/home/jschmid/reviewlette/auth'
 require '/home/jschmid/reviewlette/nameparser'
+require 'yaml'
 
+# user = Auth::Basic.new
+# user.basic_auth
+#
+# pars = Parse::Contributers.new
+# pars.get_names
+#
+# #puts $array #debug maybe store it into a file later on
+# name =  $name_list.sample
+#
+# mail = Supporter::Mailer.new
+# mail.send_email "jschmid@suse.de", :body => "#{name}"
 
-user = Auth::Basic.new
-user.basic_auth
+secret = YAML.load_file('.secrets.yml')
 
-pars = Parse::Contributers.new
-pars.get_names
+client = Octokit.Client.new (:access_token => secret[:token] )
 
-#puts $array #debug maybe store it into a file later on
-name =  $name_list.sample
-
-mail = Supporter::Mailer.new
-mail.send_email "jschmid@suse.de", :body => "#{name}"
+puts client.inspect
 
 
 
