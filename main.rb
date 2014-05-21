07f19d3ee6dfa4f52a3d158db087dcb23b01f8c8
jschmid1 <jschmid@suse.de>
2014-05-21 14:22:24 +0200

    asd

--- main.rb
+++ main.rb
@@ -3,23 +3,29 @@ require 'debugger'
 require '/home/jschmid/reviewlette/mail'
 require '/home/jschmid/reviewlette/auth'
 require '/home/jschmid/reviewlette/nameparser'
+require 'yaml'
+require 'octokit'
+
+# user = Auth::Basic.new
+# user.basic_auth
+#
+# pars = Parse::Contributers.new
+# pars.get_names
+#
+# #puts $array #debug maybe store it into a file later on
+# name =  $name_list.sample
+#
+# mail = Supporter::Mailer.new
+# mail.send_email "jschmid@suse.de", :body => "#{name}"
+#
 
 
-user = Auth::Basic.new
-user.basic_auth
 
-pars = Parse::Contributers.new
-pars.get_names
+secrets = YAML.load_file '.secrets.yml'
+client = Octokit::Client.new access_token: secrets['token']
 
-#puts $array #debug maybe store it into a file later on
-name =  $name_list.sample
-
-mail = Supporter::Mailer.new
-mail.send_email "jschmid@suse.de", :body => "#{name}"
-
-
-
-
+puts client.repos('jschmid1').first.inspect
+# puts client.repo(17440018)
 
 
 
