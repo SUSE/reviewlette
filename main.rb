994c74667d532c79ea48d9d2285c51b8f38bf9fa
jschmid1 <jschmid@suse.de>
2014-05-22 14:08:02 +0200

    added repo var

--- main.rb
+++ main.rb
@@ -17,6 +17,7 @@ require 'octokit'
 #
 # mail = Supporter::Mailer.new
 # mail.send_email "jschmid@suse.de", :body => "#{name}"
+@repo = 'jschmid1/reviewlette'
 
 secret = YAML.load_file('.secrets.yml')
 @client = Octokit::Client.new(:access_token => secret['token'])
@@ -31,44 +32,22 @@ secret = YAML.load_file('.secrets.yml')
 @eval.each do |a|
   number = a[:number]
   @pull_list_old.push(number)
-  if @pull_list_old.push(number).length < @pull_list.push(number).length
-    sha = @client.pull_request('jschmid1/reviewlette', "#{@pull_list[-1]}" )
+  sleep(10)
+  if @pull_list_old.length < @pull_list.push(number).length
+    sha = @client.pull_request("#{@repo}", "#{@pull_list[-1]}" )
     @sha_id= sha.head.sha
     name = YAML.load_file('members.yml')
-    @client.create_pull_request_comment('jschmid1/reviewlette', "#{@pull_list[-1]}", "#{name['member'].sample} is you reviewer", "#{@sha_id}", '/', 1)
+    @client.create_pull_request_comment("#{@repo}", "#{@pull_list[-1]}",
+                                        "#{name['member'].sample} is you reviewer :thumbsup:", "#{@sha_id}", '', 1)
   end
 end
 
 
-
-
-
-# client.user.rels[:repos].get.data.first.rels[:pulls].get.data.inspect
-
-
 # @client.create_pull_request('jschmid1/reviewlette', 'master', 'review_140521_test_branch', 'title', 'body')
-
-
-# @client.update_pull_request('jschmid1/reviewlette', 5, 'new title', 'updated body', 'closed')
-
-
-
-# @client.create_pull_request_comment('jschmid1/reviewlette', 5, ":thumbsup:",
-#                                     '259b7293f03f13e71cdf7825eda2c5da8b67c59d', 'README.md', 1)
-
-# puts @client.say
+# - (Array<Sawyer::Resource>) pull_request_files(repo, number, options = {}) (also: #pull_files)
 #
-# count pullrequests => if pullrequests.new > pullrequests
-#                         assign teammember to it
-#                         create pull request comment
-#                         send mail
-#                       end
-
-# will be depricated when implemented in git-review gem
-
-
+#     List files on a pull request.
 
 # @user = @client.search_users('jschmid1').inspect
-
 
 #no mehtod for asssining a teammember to a given issue
