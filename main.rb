b026f924c07f27d773465a23f28a18aa42ffe4f6
jschmid1 <jschmid@suse.de>
2014-05-26 19:16:36 +0200

    added card finder based on short_id

--- main.rb
+++ main.rb
@@ -1,53 +1,114 @@
 require 'rubygems'
 require 'debugger'
 require '/home/jschmid/reviewlette/mail'
-require '/home/jschmid/reviewlette/auth'
-require '/home/jschmid/reviewlette/nameparser'
 require 'yaml'
 require 'octokit'
+require 'trello'
 
+
+####### CONFIGURATION #############################################################
+TRELLO_CONFIG = YAML.load_file('.trellokey.yml')
+Trello.configure do |config|
+  config.developer_public_key = TRELLO_CONFIG['consumerkey']
+  config.member_token = TRELLO_CONFIG['oauthtoken']
+end
+@board = Trello::Board.find(TRELLO_CONFIG['board_id'])
 MEMBERS = YAML.load_file('members.yml')
-# mail = Supporter::Mailer.new
-
+@mail = Supporter::Mailer.new
 @repo = 'jschmid1/reviewlette'
-
-secret = YAML.load_file('.secrets.yml')
-@client = Octokit::Client.new(:access_token => secret['token'])
+GITHUB_CONFIG = YAML.load_file('.secrets.yml')
+@client = Octokit::Client.new(:access_token => GITHUB_CONFIG['token'])
 @client.user_authenticated? ? true : exit
 
+
+
+# @acard = Trello::Card.find('OpyYLJkQ')
+def find_card(title)
+  title='git_review_123123123_trello: 23'
+  re1='.*?'	# Non-greedy match on filler
+  re2='\\d+'	# Uninteresting: int
+  re3='.*?'	# Non-greedy match on filler
+  re4='(\\d+)'	# Integer Number 1
+  re=(re1+re2+re3+re4)
+  m=Regexp.new(re,Regexp::IGNORECASE)
+  if m.match(txt)
+    @id=m.match(title)[1]
+    puts @id
+  end
+  @acard = @board.cards.find{|c| c.short_id == @id}
+  debugger
+end
+
+
+def add_to_card
+  # need some logic to not assign an already assigned member to the card
+  # still broken
+  user = TRELLO_CONFIG['member'].sample
+  reviewer = @board.members.find{|m| m.username == user}
+  puts "trying to add user #{reviewer.username}"
+  @acard.add_member(reviewer)
+  @acard.add_comment("#{user}: i will review it")
+end
+
+
+def move_list(repo, number)
+  ## if reviewstatus is 'open or merged? == false' move card to inReview
+  # if reviewstatus is 'closed or merged? == true' move card to done
+  list_in_review = @board.lists.find {|x| x.name == 'in review'}
+  list_done= @board.lists.find {|x| x.name == 'Done'}
+  if (@client.pull_merged?(repo, number))
+    @acard.move_to_list(list_done.id)
+    puts "moved to #{list_done.name}"
+  else
+    @acard.move_to_list(list_in_review.id)
+    puts "moved to #{list_in_review.name}"
+  end
+end
 
 
 def assignee?(repo)
   # list issues. if noone is assigned consider it as a new issue and continue
   status = @client.list_issues("#{repo}")
   status.each do |a|
-    if !(a[:assignee])
+    unless a[:assignee]
       @number = a[:number]
       @title = a[:title]
       @body = a[:body]
-      add_assignee(@number, @title, @body)
+      # add_assignee(@number, @title, @body)
+      # move_list(@repo, @number)
+      find_card(@title)
     end
   end
 end
 
-# @client.update_issue("#{@repo}", "#{@number}", "#{@title}", nil,{:assignee => 'jschmid1'})
-
-
 
 
 def add_assignee(number, title, body)
-  #adds assignee, posts comment and sends mail
+  # adds assignee, posts comment and sends mail
   name = MEMBERS['member'].sample
-
-
-  @client.add_comment("#{@repo}", "#{number}", "#{name} is your reviewer")
-  #check if a assignee is set? catch error
-  @client.update_issue("#{@repo}", "#{number}", "#{title}", "#{body}",{:assignee => 'jschmid1'})
-  #find participants ( or edit the names file to github names => value=githubname)
-  # mail.send_email "#{notimplementedyet}", :body => "#{somegenerated text with a link to the review}"
+  debugger
+  # check if a assignee is set? catch error (TODO)
+  @client.update_issue("#{@repo}", "#{number}", "#{title}", "#{body}",{:assignee => "#{name}"})
+  @client.add_comment("#{@repo}", "#{number}", "#{name} is your reviewer :thumbsup: ")
+  # @mail.send_email "#{user => email}", :body => "#{somegenerated text with a link to the review}"
 end
 
+
 assignee?(@repo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # @client.create_pull_request('jschmid1/reviewlette', 'master', 'review_140521_test_branch', 'title', 'body')
--- trello.rb
+++ trello.rb
@@ -2,62 +2,59 @@ require 'trello'
 require 'yaml'
 require 'debugger'
 
-
 ####### CONFIGURATION #############################################################
 CONFIG = YAML.load_file('.trellokey.yml')
 Trello.configure do |config|
   config.developer_public_key = CONFIG['consumerkey']
   config.member_token = CONFIG['oauthtoken']
 end
-me =Trello::Member.find(CONFIG['member'].first)
-###################################################################################
 
 ####### INITS ##############
-board = Trello::Board.find(CONFIG['board_id'])
-# gitcard = board.cards.find{|c| c.name == provided_by_reviewname}
-## find the card based on pull-request id (TODO) # convention pls
-acard = Trello::Card.find('71qhMG11')
-## work with fake card and fake board
-## randomly choose a reviewer
-
-## choosing the right card
+@board = Trello::Board.find(CONFIG['board_id'])
 
 
-## have to use a hardcoded list because too many ppl in the board
+def find_card
+  # gitcard = board.cards.find{|c| c.name == provided_by_reviewname}
+  # find the card based on pull-request id (TODO) # convention pls
+  @acard = Trello::Card.find('71qhMG11')
+end
 
-@member=[]
-# reviewer.each do |u|
-#   @member.push(u.username)
-# end
-debugger
-###################################################################################
-while (true) do
-  #save users in arrays and shift out
-  user = CONFIG['member'].sample
-  reviewer = board.members.find{|m| m.username == user}
-  if (reviewer)
-    begin
+def move_list
+  ## if reviewstatus is 'open or merged? == false' move card to inReview
+  # if reviewstatus is 'closed or merged? == true' move card to done
+  list_inreview = @board.lists.find {|x| x.name == 'in review'}
+  list_done= @board.lists.find {|x| x.name == 'Done'}
+  (@client.pull_merged?(@repo, @pull_list[-1])) ? acard.move_to_list(list_done) : acard.move_to_list(list_inreview)
+end
+
+
+def add_to_card
+  while (true) do
+    # dont know how to catch
+    # if reviewer is already on the card, catch error and try again with another sample (TODO)
+    # loop over add card exit on success
+    # save users in arrays and shift out
+    user = CONFIG['member'].sample
+    reviewer = @board.members.find{|m| m.username == user}
+    if (reviewer)
+      begin
         puts "trying to add user #{reviewer.username}"
         acard.add_member(reviewer)
         acard.add_comment("#{user}: i will review it")
         return
-    rescue => error
-      puts error.message
+      rescue => error
+        puts error.message
+      end
+    else
+      puts "user #{user} not found in trello board"
     end
-  else
-    puts "user #{user} not found in trello board"
   end
 end
-## dont know how to catch
-## if reviewer is already on the card, catch error and try again with another sample (TODO)
-##loop over add card exit on success
 
-list_inreview = board.lists.find {|x| x.name == 'in review'}
-list_done= board.lists.find {|x| x.name == 'Done'}
-## if reviewstatus is 'closed or merged? == true' move card to done
-(@client.pull_merged?(@repo, @pull_list[-1])) ? acard.move_to_list(list_done) : acard.move_to_list(list_inreview)
 
-## if reviewstatus is 'open or merged? == false' move card to inReview
+
+
+
 
 
 #### STRATEGY #####
