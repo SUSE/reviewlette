671da5e95c630fe7fef7ed037ce5bf4378af375c
jschmid1 <jschmid@suse.de>
2014-05-26 17:08:10 +0200

    updated member lis

--- main.rb
+++ main.rb
@@ -6,50 +6,48 @@ require '/home/jschmid/reviewlette/nameparser'
 require 'yaml'
 require 'octokit'
 
-
+MEMBERS = YAML.load_file('members.yml')
 # mail = Supporter::Mailer.new
-# mail.send_email "jschmid@suse.de", :body => "#{name}"
-repo = 'jschmid1/reviewlette'
+
+@repo = 'jschmid1/reviewlette'
 
 secret = YAML.load_file('.secrets.yml')
 @client = Octokit::Client.new(:access_token => secret['token'])
 @client.user_authenticated? ? true : exit
 
+
+
 def assignee?(repo)
   # list issues. if noone is assigned consider it as a new issue and continue
-
   status = @client.list_issues("#{repo}")
   status.each do |a|
-    if !(a[:assignee]) == ''
-      eval= @client.pull_requests("#{repo}")
-      @pull_list = []
-      eval.each do |x|
-        number = x[:number]
-        @pull_list.push(number)
-      end
-      add_assignee(@pull_list[-1])
+    if !(a[:assignee])
+      @number = a[:number]
+      @title = a[:title]
+      @body = a[:body]
+      add_assignee(@number, @title, @body)
     end
   end
 end
 
+# @client.update_issue("#{@repo}", "#{@number}", "#{@title}", nil,{:assignee => 'jschmid1'})
 
-debugger
-def add_assignee(number)
+
+
+
+def add_assignee(number, title, body)
   #adds assignee, posts comment and sends mail
+  name = MEMBERS['member'].sample
 
-  name = YAML.load_file('members.yml')
-  # need this to comment on specific files
-  # sha = @client.pull_request("#{@repo}", "#{number}" )
-  # sha_id= sha.head.sha
-  # @client.pull_request_files("#{repo}, #{number}")
 
   @client.add_comment("#{@repo}", "#{number}", "#{name} is your reviewer")
-  @client.update_issue("#{@repo}", "#{number}", 'title', nil,{:assignee => "#{name}"})
+  #check if a assignee is set? catch error
+  @client.update_issue("#{@repo}", "#{number}", "#{title}", "#{body}",{:assignee => 'jschmid1'})
   #find participants ( or edit the names file to github names => value=githubname)
   # mail.send_email "#{notimplementedyet}", :body => "#{somegenerated text with a link to the review}"
 end
 
-
+assignee?(@repo)
 
 
 # @client.create_pull_request('jschmid1/reviewlette', 'master', 'review_140521_test_branch', 'title', 'body')
@@ -59,4 +57,7 @@ end
 
 # @user = @client.search_users('jschmid1').inspect
 
-assignee?(repo)
+# need this to comment on specific files
+# sha = @client.pull_request("#{@repo}", "#{number}" )
+# sha_id= sha.head.sha
+# @client.pull_request_files("#{repo}, #{number}")
