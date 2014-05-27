742638a539932577378fc1121aae9557a16cff5f
jschmid1 <jschmid@suse.de>
2014-05-27 10:09:22 +0200

    rdoc added

--- main.rb
+++ main.rb
@@ -4,6 +4,12 @@ require '/home/jschmid/reviewlette/mail'
 require 'yaml'
 require 'octokit'
 require 'trello'
+require 'rdoc/rdoc'
+
+
+## Need to log in case of errors
+
+
 
 
 ####### CONFIGURATION #############################################################
@@ -22,7 +28,8 @@ GITHUB_CONFIG = YAML.load_file('.github.yml')
 @client.user_authenticated? ? true : exit
 
 
-
+# Finds a card due to naming convention
+# Review_1337_name_of_pr_trello_shortid_454
 def find_card(trelloid)
   re1='.*?'	# Non-greedy match on filler
   re2='\\d+'	# Uninteresting: int
@@ -30,7 +37,6 @@ def find_card(trelloid)
   re4='(\\d+)'	# Integer Number 1
   re=(re1+re2+re3+re4)
   m=Regexp.new(re,Regexp::IGNORECASE)
-
   if m.match(trelloid)
     @id=m.match(trelloid)[1]
     puts "found card nr: #{@id}"
@@ -39,28 +45,28 @@ def find_card(trelloid)
   acard
 end
 
-
-
+# Finds member by id
 def find_member_by_id(id)
   @board.members.find{|m| m.id == id}
 end
 
-
-
+# Finds member by username
 def find_member_by_username(username)
   @board.members.find{|m| m.username == username}
 end
 
 
-
+# Adds a reviewer the trello card (found by (find_card))
 def add_reviewer_to_card(card)
   assignees = card.member_ids.map{|id| find_member_by_id(id)}
   members = TRELLO_CONFIG['member'].map{|name| find_member_by_username(name) }
   available_ids = members.map(&:id) - assignees.map(&:id)
   reviewer = available_ids.map{|id| find_member_by_id(id)}.sample
+  # removes already assigned==owner of the card from the reviewers list
   if reviewer
     card.add_member(reviewer)
     card.add_comment("#{reviewer.username} will review it")
+    puts "added #{reviewer} to the card"
     return true
   else
     puts "No available reviewer found"
@@ -68,36 +74,33 @@ def add_reviewer_to_card(card)
   false
 end
 
-
+# Automatically moves the card to the right column.
 def move_card_to_list(card, repo, number)
-  ## if reviewstatus is 'open or merged? == false' move card to inReview
-  # if reviewstatus is 'closed or merged? == true' move card to done
-
   if (pull_merged?(repo, number))
+    # If reviewstatus is closed or merged move card to done
     list_done= @board.lists.find {|x| x.name == 'Done'}
     card.move_to_list(list_done.id)
     puts "moved to #{list_done.name}"
   else
+    ## if reviewstatus is open or merged move card to inReview
     list_in_review = @board.lists.find {|x| x.name == 'in review'}
     card.move_to_list(list_in_review.id)
     puts "moved to #{list_in_review.name}"
   end
 end
 
-
+# List issues. If noone is assigned, consider it as a new issue.
 def assignee?(repo)
-  # list issues. if noone is assigned, consider it as a new issue and continue
   status = @client.list_issues("#{repo}")
   status.each do |a|
     unless a[:assignee]
       @number = a[:number]
       @title = a[:title]
       @body = a[:body]
-
       card = find_card(@title)
-
+      # Only proceed when a card was found
       if card
-        # add_assignee(@number, @title, @body)
+        add_assignee(@number, @title, @body)
         move_card_to_list(card, @repo, @number) if add_reviewer_to_card(card)
       else
         puts "Card not found for title #{@title.inspect}"
@@ -106,12 +109,14 @@ def assignee?(repo)
   end
 end
 
-
+# Checks if the pull is merged
 def pull_merged?(repo, number)
+  #check if pull is merged
   @client.pull_merged?(repo, number)
 end
 
 
+# Adds an assignee to an Github-issue
 def add_assignee(number, title, body)
   # adds assignee, posts comment and sends mail
   name = MEMBERS['member'].sample
@@ -124,19 +129,3 @@ end
 
 
 assignee?(@repo)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
