89b0ca46e9cd57609f3011e7465e906b9e790e8c
jschmid1 <jschmid@suse.de>
2014-05-27 13:23:29 +0200

    rspec tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
+require 'home/jschmid/reviewlette/main.rb'
 
-describe "Find" do
 
-  before :each do
-    @card = @board.cards.first
+
+describe Github do
+  subject { Github.new() }
+  describe "#init" do
+    it "creates a new Github init object" do
+      subject.should be_an_instance_of Github
+    end
   end
-  describe "gets a card" do
 
+
+  describe "#assignee?" do
+    it "checks for an unassigend Issue" do
+      subject.should be_an_instance_of Github
+    end
   end
 
+  describe "#pull_merged?" do
+    it "checks if pull is merged" do
+      subject.pull_merged?(repo, number).should eql (false||true)
+    end
+  end
 end
