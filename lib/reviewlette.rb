30f7a87b0bb53f9e553469a94a1e793f91e0e3a0
Thomas Schmidt <tschmidt@suse.de>
2015-04-08 16:19:43 +0200

    bump version

--- lib/reviewlette.rb
+++ lib/reviewlette.rb
@@ -3,7 +3,7 @@ require 'reviewlette/github_connection'
 require 'reviewlette/vacations'
 require 'yaml'
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 MEMBERS_CONFIG = YAML.load_file("#{File.dirname(__FILE__)}/../config/members.yml")
 
 class Reviewlette
