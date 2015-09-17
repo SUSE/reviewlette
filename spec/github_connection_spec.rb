206d8a984105ce79160e8737363f8581a1702eef
Felix Schnizlein <felix@none.io>
2015-09-17 17:03:19 +0200

    Changed test according to my changes

--- spec/github_connection_spec.rb
+++ spec/github_connection_spec.rb
@@ -2,22 +2,21 @@ require 'spec_helper'
 
 describe GithubConnection do
 
-  GITHUB_CONFIG = { token: '661', repo: 'SUSE/reviewlette' }
   subject { GithubConnection }
-  let( :connection ) { subject.new }
+  let( :connection ) { subject.new(repo, token) }
+  let( :repo ) { "test" }
+  let( :token ) { "foo" }
 
   describe '.new' do
-
     it 'initializes octokit client and repo' do
-      expect(Octokit::Client).to receive(:new).with(:access_token => GITHUB_CONFIG['token'])
-      connection = subject.new
-      expect(connection.repo).to eq(GITHUB_CONFIG['repo'])
+      expect(Octokit::Client).to receive(:new).with(:access_token => token)
+      expect(connection.repo).to eq(repo)
     end
   end
 
   describe '#add_assignee' do
     it 'adds an assignee to the gh issue' do
-      expect(connection.client).to receive(:update_issue).with(connection.repo, 11, :assignee => 'test')
+      expect(connection.client).to receive(:update_issue).with(repo, 11, :assignee => 'test')
       connection.add_assignee(11, 'test')
     end
   end
@@ -26,16 +25,23 @@ describe GithubConnection do
     it 'comments on a given issue' do
       card = Trello::Card.new
       allow(card).to receive(:url).and_return('url')
-      expect(connection.client).to receive(:add_comment).with(connection.repo, 11, anything)
+      expect(connection.client).to receive(:add_comment).with(repo, 11, anything)
       connection.reviewer_comment(11, 'test', card)
     end
   end
 
   describe '#list_pulls' do
     it 'lists all pullrequests for a given repository' do
-      expect(connection.client).to receive(:pull_requests).with(connection.repo)
+      expect(connection.client).to receive(:pull_requests).with(repo)
       connection.list_pulls
     end
   end
 
+  describe '#repo_exists?' do
+    it 'checks if a certain repository exists' do
+      expect(connection.client).to receive(:repository?).with(repo)
+      connection.repo_exists?
+    end
+
+  end
 end
--- spec/reviewlette_spec.rb
+++ spec/reviewlette_spec.rb
@@ -4,44 +4,53 @@ describe Reviewlette do
 
   subject { Reviewlette }
   let( :reviewlette ) { subject.new }
+  let( :token ) { "1234" }
+  let( :repo ) { "SUSE/test" }
 
   before {
-    allow(GithubConnection).to receive(:new).and_return GithubConnection
     allow(TrelloConnection).to receive(:new).and_return TrelloConnection
+    allow(GithubConnection).to receive(:new).with(repo, token).and_return GithubConnection
   }
 
 
   describe '.new' do
-    it 'sets github and trello connections' do
-       expect(GithubConnection).to receive (:new)
+    it 'sets trello connections' do
        expect(TrelloConnection).to receive (:new)
        subject.new
     end
   end
 
   describe '.run' do
+    it 'iterates over all open repositories and looks for unassigned pull requests' do
+        expect(reviewlette).to receive(:check_repo).with(repo, token)
+        reviewlette.check_repo(repo, token)
+    end
+  end
 
+  describe '.check_repo' do 
     it 'iterates over all open pull requests and extracts trello ids from name' do
+
+      expect(GithubConnection).to receive(:repo_exists?).and_return true
       expect(GithubConnection).to receive(:unassigned_pull_requests).and_return([{number: 11, title: 'test_issue_12'}])
       expect(TrelloConnection).to receive(:find_card_by_id).with(12)
 
-      reviewlette.run
+      reviewlette.check_repo(repo, token)
     end
 
     it 'adds assignee and reviewer comment on github, adds comment on trello and moves card' do
       card = Trello::Card.new
+      expect(GithubConnection).to receive(:repo_exists?).and_return true
       expect(GithubConnection).to receive(:unassigned_pull_requests).and_return([{number: 11, title: 'test_issue_12'}])
       expect(TrelloConnection).to receive(:find_card_by_id).with(12).and_return(card)
       expect(reviewlette).to receive(:select_reviewer).and_return({'suse_username' => 'test', 'github_username' => 'testgit'})
 
       expect(GithubConnection).to receive(:add_assignee).with(11, 'testgit')
       expect(GithubConnection).to receive(:reviewer_comment).with(11, 'testgit', card)
-      expect(GithubConnection).to receive(:repo).and_return('SUSE/test')
 
       expect(TrelloConnection).to receive(:comment_on_card).with("@ will review https://github.com/SUSE/test/issues/11", card)
       expect(TrelloConnection).to receive(:move_card_to_list).with(card, 'In review')
 
-      reviewlette.run
+      reviewlette.check_repo(repo, token)
     end
 
   end
