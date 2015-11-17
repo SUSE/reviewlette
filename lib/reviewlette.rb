2d9efb6b4e372e8dbbb119f39d5ce96b3400af51
Felix Schnizlein <felix@none.io>
2015-11-17 19:22:09 +0100

    Chaged card id detection to regex and added more tests

--- lib/reviewlette.rb
+++ lib/reviewlette.rb
@@ -32,7 +32,14 @@ class Reviewlette
       issue_title = issue[:title]
 
       puts "*** Checking unassigned github pull request: #{issue_title}"
-      card_id = issue_title.split(/[_ -#\.]/).last.to_i
+      matched = issue_title.match(/\d+[_'"]?$/)
+
+      unless matched
+        puts "Pull request not assigned to a trello card"
+        next
+      end
+
+      card_id = matched[0].to_i
       card    = @trello.find_card_by_id(card_id)
 
       unless card
